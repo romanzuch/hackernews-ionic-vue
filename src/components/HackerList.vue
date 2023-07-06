209323539fd089cb64af5a931a63b4753cdb3e53
romanzuch <roman.zuchowski@gmail.com>
2023-07-06 17:36:50 +0200

    this will add barebone list component

---
+++ src/components/HackerList.vue
@@ -0,0 +1,26 @@
+<script setup lang='ts'>
+    import { 
+        IonItem, 
+        IonLabel, 
+        IonList 
+    } from '@ionic/vue';
+</script>
+
+<style scoped lang='scss'>
+    .list-container {
+        padding: 0 env(safe-area-inset-left);
+    }
+</style>
+
+<template>
+    <div class='list-container'>
+        <ion-list>
+            <ion-item>
+                <ion-label>1</ion-label>
+            </ion-item>
+            <ion-item>
+                <ion-label>2</ion-label>
+            </ion-item>
+        </ion-list>
+    </div>
+</template>
