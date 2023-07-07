f677bf7783cd0648c516f000e4e93a9fd789e16f
romanzuch <roman.zuchowski@gmail.com>
2023-07-07 16:43:30 +0200

    feature/best
    this will convert the Story interface to a class

--- src/models/Story.ts
+++ src/models/Story.ts
@@ -1,4 +1,4 @@
-interface Story {
+class Story {
     by: string;
     descendants: number;
     id: number;
@@ -8,6 +8,18 @@ interface Story {
     title: string;
     type: string;
     url: string;
+
+    constructor() {
+        this.by = "";
+        this.descendants = 0;
+        this.id = 0;
+        this.kids = [0];
+        this.score = 0;
+        this.time = 0;
+        this.title = "";
+        this.type = "";
+        this.url = "";
+    }
 }
 
 export default Story;
